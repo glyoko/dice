4bbe37a415c97a207c8d5d0f773be5f49cbe33d2
Adam Braman <glyoko1@gmail.com>
2017-09-29 05:41:59 -0700

    Create memoized version of dice-prob method

--- src/dice/dice.clj
+++ src/dice/dice.clj
@@ -1,18 +1,18 @@
 (ns dice.dice
   (:gen-class))
 
-(declare dice-prob)
+(declare dice-prob dice-prob-memo)
 
 
 (defn sum-prev-probs
 	[die-count number]
 	(+
-		(dice-prob die-count (- number 1))
-		(dice-prob die-count (- number 2))
-		(dice-prob die-count (- number 3))
-		(dice-prob die-count (- number 4))
-		(dice-prob die-count (- number 5))
-		(dice-prob die-count (- number 6))))
+		(dice-prob-memo die-count (- number 1))
+		(dice-prob-memo die-count (- number 2))
+		(dice-prob-memo die-count (- number 3))
+		(dice-prob-memo die-count (- number 4))
+		(dice-prob-memo die-count (- number 5))
+		(dice-prob-memo die-count (- number 6))))
 
 (defn dice-prob
 	[die-count, number]
@@ -21,3 +21,5 @@
 			1/6
 			0)
 		(* 1/6 (sum-prev-probs (- die-count 1) number))))
+
+(def dice-prob-memo (memoize dice-prob))
